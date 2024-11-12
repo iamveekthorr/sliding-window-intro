7ce37c5dda03e582b57b57e385d6efda0c062b3d
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2024-11-12 16:14:32 +0100

    chore: initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let nums = [3, 1, 2, 7, 4, 2, 1, 1, 5];
+    let k = 8;
+
+    let longest_sum = longest_sum_of_sub_arrays(&nums, &k);
+
+    println!(
+        "The longest sum of a subarray where actual sum equal {} is: {}",
+        k, longest_sum
+    );
 }
 
-fn longest_sum_of_sub_arrays(nums: &[i32], k: &i32) -> i32 {
+fn longest_sum_of_sub_arrays(nums: &[i32], k: &i32) -> usize {
     let mut left = 0;
     let mut current = 0;
     let mut ans = 0;
@@ -16,7 +24,7 @@ fn longest_sum_of_sub_arrays(nums: &[i32], k: &i32) -> i32 {
             left += 1;
         }
 
-        ans = ans.max(current)
+        ans = ans.max(right - left + 1);
     }
 
     ans
